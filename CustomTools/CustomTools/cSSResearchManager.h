a1bbb014931f9ee732fef2eea51baa7c695c9f3e
Chocice75 <35960636+VanillaCold@users.noreply.github.com>
2023-08-27 00:15:48 +0100

    Add ResearchType struct

--- CustomTools/CustomTools/cSSResearchManager.h
+++ CustomTools/CustomTools/cSSResearchManager.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <Spore\BasicIncludes.h>
+#include <exception>
+#include <stdexcept>
 
 #define cSSResearchManagerPtr intrusive_ptr<cSSResearchManager>
 #define SSResearchManager (cSSResearchManager::Get())[0]
@@ -9,6 +11,45 @@
 /// In your dllmain Initialize method, add the system like this:
 /// ModAPI::AddSimulatorStrategy(new cSSResearchManager(), cSSResearchManager::NOUN_ID);
 ///
+
+struct ResearchType
+{
+public:
+	ResearchType(uint32_t propID)
+	{
+		if (PropManager.GetPropertyList(propID, id("SS-research"), mpPropList))
+		{
+			App::Property::GetUInt32(mpPropList.get(), id("ResearchTool"), mToolID);
+
+			size_t count;
+			uint32_t* ids;
+			App::Property::GetArrayUInt32(mpPropList.get(), id("ResearchDependancies"), count, ids);
+			if (count != 0)
+			{
+				for (int i = 0; i < count; i++)
+				{
+					mPriorResearches.push_back(ids[i]);
+				}
+			}
+
+			if (!App::Property::GetUInt32(mpPropList.get(), id("ResearchPointsRequired"), mRequiredPoints))
+			{
+				string error = "Research property list " + to_string(propID) + " has no required point count!";
+				throw std::invalid_argument(error.c_str());
+			}
+		}
+		else
+		{
+			string error = "Research property list "+to_string(propID)+" is not valid!";
+			throw std::invalid_argument(error.c_str());
+		}
+	}
+
+	PropertyListPtr mpPropList;
+	uint32_t mToolID;
+	vector<uint32_t> mPriorResearches;
+	uint32_t mRequiredPoints;
+};
 
 class cSSResearchManager
 	: public Simulator::cStrategy
@@ -33,6 +74,8 @@ public:
 
 	static Simulator::Attribute ATTRIBUTES[];
 	static cSSResearchManager* Get();
+
+	vector<ResearchType> researches;
 
 private:
 	static cSSResearchManager* sInstance;
