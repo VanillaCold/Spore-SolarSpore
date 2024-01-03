6ef1451418071d39f2c40523e64e84dd82dd8b5d
Chocice75 <35960636+VanillaCold@users.noreply.github.com>
2024-01-03 12:38:00 +0000

    Add comments to DelayedBeamWeapon

--- CustomTools/CustomTools/DelayedBeamWeapon.cpp
+++ CustomTools/CustomTools/DelayedBeamWeapon.cpp
@@ -15,13 +15,15 @@ DelayedBeamWeapon::~DelayedBeamWeapon()
 void DelayedBeamWeapon::OnMouseDown(Simulator::cSpaceToolData* pTool, const Vector3& playerPosition)
 {
 
-	if (!mFireTimer.IsRunning())
+	if (!mFireTimer.IsRunning()) //If the timer's not running,
 	{
+		//start the timer.
 		toReset = true;
 		mFireTimer.Start();
 	}
-	else
+	else //Otherwise,
 	{
+		//if enough time has passed, then call the base function.
 		if (mFireTimer.GetElapsedTime() > 1500)
 		return Simulator::cDefaultBeamTool::OnMouseDown(pTool, playerPosition);
 	}
@@ -31,27 +33,37 @@ void DelayedBeamWeapon::OnMouseDown(Simulator::cSpaceToolData* pTool, const Vect
 
 bool DelayedBeamWeapon::Update(Simulator::cSpaceToolData* pTool, bool showErrors, const char16_t** ppFailText)
 {
+	//Just call the base function. maybe add more stuff here in future.
 	return Simulator::cToolStrategy::Update(pTool, showErrors, ppFailText);
 }
 
 bool DelayedBeamWeapon::WhileFiring(Simulator::cSpaceToolData* pTool, const Vector3& aimPoint, int unk)
 {
+	//If the timer's not running,
 	if (!mFireTimer.IsRunning())
 	{
+		//set the timer mode to milliseconds
 		mFireTimer.SetMode(Clock::Mode::Milliseconds);
+		//And then start the timer.
 		toReset = true;
 		mFireTimer.Start();
 	}
 
-	App::ConsolePrintF("%i", mFireTimer.GetElapsedTime());
+	SporeDebugPrint("%i", mFireTimer.GetElapsedTime());
+	//If the elapsed time is past 1.5 seconds,
 	if (mFireTimer.GetElapsedTime() > 1500)
 	{
+		//Call the base function.
 		Simulator::cDefaultBeamTool::WhileFiring(pTool, aimPoint, unk);
 		if (toReset)
+			//If it's the first time going past the 1.5 second limit,
 		{
+			//call the base function for OnMouseDown() as well.
 			Simulator::cDefaultBeamTool::OnMouseDown(pTool, aimPoint);
+			//And make sure we don't do this again.
 			toReset = false;
 		}
+		//Return true to ensure that no weird shenanigans ensue.
 		return true;
 	}
 	return false;
@@ -59,7 +71,10 @@ bool DelayedBeamWeapon::WhileFiring(Simulator::cSpaceToolData* pTool, const Vect
 
 bool DelayedBeamWeapon::OnMouseUp(Simulator::cSpaceToolData* pTool)
 {
+	//Just stop the timer,
 	mFireTimer.Reset();
+	//set the value for if we've not yet gone past 1.5s before to true
 	toReset = true;
+	//and return the base function.
 	return Simulator::cDefaultBeamTool::OnMouseUp(pTool);
 }
